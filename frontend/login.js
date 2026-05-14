0a126ea4b006883c0bfbc7666646424beb50c0c8
bagumarikrishna-dev <bagumarikrishna@gmail.com>
2026-05-14 15:01:59 +0530

    updated frontend

--- frontend/login.js
+++ frontend/login.js
@@ -3,7 +3,7 @@ function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if(username === "krishnaveni@gmail.com" && password === "08") {
+    if(username === "krishnaveni" && password === "08") {
 
         window.location.href = "index.html";
 
